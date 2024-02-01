830eba98a877d0cf3988ba2da5840ea1813944ac
Ricardo Rafael Quintero Meza, PhD <84739791+ricardo-qm@users.noreply.github.com>
2024-02-01 16:03:12 +0000

    Añadir ventana y etiqueta "Hola Mundo" en miPrincipal.Principal.java

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,7 +1,21 @@
 package miPrincipal;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Principal {
+
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        // Crear y configurar la ventana
+        JFrame frame = new JFrame("Hola Mundo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+
+        // Añadir la etiqueta "Hola Mundo"
+        JLabel label = new JLabel("Hola Mundo");
+        frame.getContentPane().add(label);
+
+        // Mostrar la ventana
+        frame.setVisible(true);
     }
 }
